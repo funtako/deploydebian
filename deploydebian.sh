d1c2dd1ddac58b1de8ff0ecf6cb57e48deecfa00
funtako <88504814+funtako@users.noreply.github.com>
2023-05-19 14:13:30 +0100

    Add files via upload
    added more things to install

--- deploydebian.sh
+++ deploydebian.sh
@@ -11,6 +11,7 @@
 #doom emacs?
 #GET A FILE THAT WILL JUST LOG THE OUTPUT OF THE SCRIPT
 
+
 #update and install all updates
 sudo apt update
 sudo apt upgrade -y
@@ -18,7 +19,8 @@ sudo apt upgrade -y
 sucklessloc=$HOME/suckless
 dotfilesloc=$HOME/funtako/dot-files
 
-
+#make some sudo commands not need password like shutdown reboot and others
+echo "$USER ALL = NOPASSWD:/sbin/reboot, /sbin/shutdown" | sudo tee -a /etc/sudoers
 
 #create directory for screenshots folder that will be used with maim and my scripts and sxhkd
 mkdir -p $HOME/pictures/screenshots
@@ -35,16 +37,20 @@ mkdir $HOME/music
 sudo apt install alsa-utils pulseaudio -y
 
 # install some important programs
-sudo apt-get install -y vim xinit git build-essential wget curl
+sudo apt-get install -y neovim xinit git build-essential wget curl
 
-#unzip .zip files
-sudo apt install unzip -y
+
+#p7zip-full aka 7za for unzipping 7z and loads of other types of files like zip files
+sudo apt install p7zip-full
 
 #install rsync - allows me to copy hidden .files properly 
 sudo apt install rsync -y
 
 # install dependencies for suckless dwm
 sudo apt-get install -y libx11-dev libxft-dev libxinerama-dev
+
+#dependency for slock
+sudo apt install libxrandr-dev -y
 
 
 #FONTS################
@@ -55,7 +61,7 @@ cd $HOME/.fonts
 #download ubuntu nerd fonts # link from https://www.nerdfonts.com/font-downloads
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Ubuntu.zip
 #unzip the Ubuntu.zip file
-unzip Ubuntu.zip
+7za e Ubuntu.zip
 #remove the zip file now
 rm Ubuntu.zip
 #refresh font cache so progrmas that use fontconfig can show available fonts on the system - forcefully and -verbose
@@ -156,10 +162,6 @@ sudo apt install foremost -y
 #calibre ebook manager and format conversion (bloated)
 sudo apt install calibre -y
 
-#TODO: might need also to get python3 and also might need to create a symlink from the folder python to python3?
-#python3-pip python package installer
-sudo apt install python3-pip -y
-
 
 #figlet just lets you make cool looking big text in terminal
 sudo apt install figlet -y
@@ -210,14 +212,19 @@ sudo apt install mpdris2 -y
 #notify-send for creating custom notifications libnotify-bin
 sudo apt instlal libnotify-bin -y
 sudo apt install dunst -y
+#allows notify-send to be used from crontab
+sudo apt install dbus-x11
 ####################
 
-#youtube-dl 
-sudo apt install youtube-dl -y
 
+
+
+#terminal pdf reader
+sudo apt install zathura -y
 
 #gui torrent client qbittorrent
 sudo apt install qbittorrent -y
+
 
 #fuzzy FuZzy finder - lets you quickly search to find a file
 sudo apt install fzf -y
@@ -241,7 +248,64 @@ figlet check version of GO this might need to be changed and check github page f
 #now move lf from the folder in home to /usr/bin
 sudo cp $HOME/go/bin/lf /usr/bin
 
+#zsh shell has more features than bash shell
+sudo apt install zsh -y
+
+#get the fast-syntax-highlighting plugin and put it in that folder, this is at the bottom of ~/.zshrc
+#means that incorrect commands will be red and correct will be green
+sudo git clone https://github.com/zdharma-continuum/fast-syntax-highlighting /usr/share/zsh/plugins/fast-syntax-highlighting/
+
+#change default shell to zsh
+chsh -s $(which zsh)
 
 
 
+#TODO: might need also to get python3 and also might need to create a symlink from the folder python to python3?
+#python3-pip python package installer
+sudo apt install python3-pip -y
+#venv virtual environment needed for pipx 
+sudo apt install python3-venv
+#pipx vritual environment pip for installing stig torrent client
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+#install stig with pipx self contained virtual environment
+pipx install stig
+#yt-dlp  - fork of youtube-dl
+pipx install yt-dlp
+#spotdl downloads songs from youtube but gives you the spotify metadata
+pipx install spotdl
+#TODO try that undetected webdriver python bot on this computer then put the install for undetected webdriver in here maybe for pipx if not then just for pip
+
+
+#torrent daemon
+sudo apt install transmission-daemon
+
+#rss reader
+sudo apt install newsboat -y 
+
+#to do list and calender
+sudo apt install calcurse -y
+
+#spanish english dictioary
+sudo apt install dictd -y
+sudo apt install dict-freedict-spa-eng -y
+sudo apt install dict-freedict-eng-spa -y
+
+
+#MIGHT BE A GOOD IDEA TO GET POSTFIX set it to offline tho so u get mail for when ur cron fails in /var/mail
+
+#timeshift to restore snapshots if something is broken still needs to be configured
+sudo apt install timeshift -y
+
+
+
+figlet still got to get crontabs and put them in can be found sudo nvim /var/spool/cron/crontabs
+
+
+#NEEDED for mounting the samba share to your etc/fstab file
+sudo apt install cifs-utils
+
+#TODO manually you need to add your samba shares to fstab
+#mount the drive to /media/$USER/FOLDERNAMEORUFCHOICE
+#insie of pcmanfm add the mounted directory to bookmarks
 
